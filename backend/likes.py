fbf70e53b4f714ae3f6e0e78d431e683e65cd2d2
grihapro <gubenya04@mail.ru>
2024-05-01 20:39:44 +0300

    add notifications

--- backend/likes.py
+++ backend/likes.py
@@ -2,18 +2,17 @@ from datetime import date
 from options import app
 from pydantic import BaseModel
 from database import db, Like
-from fastapi.responses import JSONResponse
+from functions import birthday_to_age
 
 
 @app.post("/likeAnket")
 def liked(data: Like):
-    sympathy = False
     try:
         list_likes = list(db.child("likes").get().val()[data.userId])
     except:
         list_likes = []
     if list_likes.count(data.otheruserId):
-        return JSONResponse(content={"message": "Запрос уже отправлен"}, status_code=403)
+        return data
     else:
         try:
             if list(db.child("likes").get().val()[data.otheruserId]).count(data.userId) and db.child("likes").get().val():
@@ -29,14 +28,30 @@ def liked(data: Like):
                     list_sympathies = []
                 list_sympathies.append(data.userId)
                 db.child("sympathies").update({data.otheruserId: list_sympathies})
-                sympathy = True
+                try:
+                    notes = db.child("notifications").get().val()[data.otheruserId]
+                except:
+                    notes = []
+                note = db.child("userInfo").child(data.userId).get().val()
+                notification = {"notificationId": len(notes) + 1,
+                                "message": f"У вас взаимная симпатия с {note['firstname']}, {birthday_to_age(note['birthday'])}!"}
+                notes.append(notification)
+                db.child("notifications").child(data.otheruserId).set(notes)
+            else:
+                try:
+                    notes = db.child("notifications").get().val()[data.otheruserId]
+                except:
+                    notes = []
+                note = db.child("userInfo").child(data.userId).get().val()
+                notification = {"notificationId": len(notes) + 1,
+                                "message": f"{note['firstname']}, {birthday_to_age(note['birthday'])} лайкнул вашу анкету!"}
+                notes.append(notification)
+                db.child("notifications").child(data.otheruserId).set(notes)
         except:
             pass
         list_likes.append(data.otheruserId)
         db.child("likes").update({data.userId: list_likes})
-        if sympathy:
-            return JSONResponse(content={"message": "Взаимная симпатия"}, status_code=203)
-        return JSONResponse(content={"message": "Good!"}, status_code=200)
+        return data
 
 
 @app.post("/dislikeAnket")
@@ -88,3 +103,24 @@ class Contacts(BaseModel):
 def user_contacts(user_id: str):
     data = db.child(f"userInfo/{user_id}").get().val()["contacts"]
     return Contacts(**data)
+
+
+class Notes(BaseModel):
+    notifications: list = []
+
+
+@app.get("/notificationsList/{user_id}")
+def notifications_list(user_id: str):
+    try:
+        notes = list(db.child("notifications").child(user_id).get().val())
+    except:
+        notes = []
+    data = Notes()
+    data.notifications = notes
+    return data
+
+
+@app.put("/notificationsRemove/{user_id}")
+def notifications_remove(user_id: str):
+    db.child("notifications").remove(user_id)
+    return user_id
